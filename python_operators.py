268680e6e507c670ed961f2ac570d64403f0c9fd
unknown <sasanksekharbaral123@gmail.com>
2024-10-07 18:00:47 +0530

    using Arithmetic operators

--- python_operators.py
+++ python_operators.py
@@ -1,2 +1,19 @@
+'''
+1. Operators are used for performing some operation on operands.
+2. In python we have 7 types of operator such as Assignment operator,Arithmetic operator,logical operaror,Relational operator,
+   Bitwise operator,membership operator,identity operator.
+
+
+'''
 a='sasank'
-print(a)
+b=3
+#print(a+b)Error
+c='3'
+print(a+c)#cdt + cdt concardination
+# substracton between two set
+s={78,56,23}
+s2={45,55,53}
+print(s-s2)#based on base set op will be returned
+#multiplication num*cdt = concardinate
+print(b*a)
+print(a*b)
