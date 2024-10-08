a2010c8491038dde447cf6dd12779f2aa6ac0950
unknown <sasanksekharbaral123@gmail.com>
2024-10-08 11:38:31 +0530

    Logical operator and it's example

---
+++ logical_operator.py
@@ -0,0 +1,20 @@
+"""
+Logical operator
+1.Logical operator is used for returning boolean value as output.
+2.These logical operator is used in conditional statements in python.
+"""
+# Logical and operator
+print([] and '')
+print([] and '1')
+print("''" and '5')
+print('5' and "''")#"''" True
+print('j' and [] )
+#Logical or operator
+print([] or {})
+print({} or '5')
+print(5 or '')
+print({4} or {2})
+# logical not operator
+print(not 5)
+print(not 0)
+print(not "{}")
