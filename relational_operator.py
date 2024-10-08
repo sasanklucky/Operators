55024191b4dec4c61b58bd826fac9762d5dc73bc
unknown <sasanksekharbaral123@gmail.com>
2024-10-08 11:09:26 +0530

    Relational operator with some example

---
+++ relational_operator.py
@@ -0,0 +1,16 @@
+"""
+Relational Operator:
+1.Relational operator is used for returning boolean value as output,
+2.these relational operator is used in conditional statements.
+3.num to num it compares easily as compare to cdt to cdt.
+4.when we compare with string,it will be done by ascii value,
+5.cdt to cdt it will compare first element of cdt.
+"""
+# Relational Operator
+a=5
+b=10
+print(a>b)
+a='sasank'
+b='lucky'
+print(a>b)#it is done by comparing first charecter of cdt
+
