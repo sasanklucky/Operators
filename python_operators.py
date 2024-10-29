2152af820ad52ae2962a943b9252181def221df0
unknown <sasanksekharbaral123@gmail.com>
2024-10-29 17:29:01 +0530

    Some Modification on Arithmetic Operator

--- python_operators.py
+++ python_operators.py
@@ -11,13 +11,26 @@ b=3
 #print(a+b)Error
 c='3'
 print(a+c)#cdt + cdt concardination
+print([45,25,36,32]+[78,45,23])
+print((45,23,650)+(45,78))
+#print({12,56}+{13,89})error addition between two set can not be performed.
+#print({'name':'sasank'}+{'age':25}) addition between two dict won't perform.
+
 # substracton between two set
 s={78,56,23}
-s2={45,55,53}
+s2={45,55,23}
 print(s-s2)#based on base set op will be returned
+
+#print('sasank'-5)Error
+#print('sasank'-'lucky')Error
+
 #multiplication num*cdt = concardinate
 print(b*a)
 print(a*b)
+print([78,12]*2)
+print((56,23,12)*3)
+#print({45,23}*3)error
+#print({'name':'sasank'}*2)Error
 #set and dict can not be performed
 #** power operator
 a=4
